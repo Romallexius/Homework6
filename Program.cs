754eefca5c24ef1be467fa3a4b2855d025244219
Aleksey Romanov <romallex@gmail.com>
2022-11-28 22:54:45 +0200

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 /*
-Console.WriteLine("Введите элементы через пробел");
+Console.WriteLine("Введите числа через пробел");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
 for (int i = 0; i < arr.Length; i++)
